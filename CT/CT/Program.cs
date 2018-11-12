32d2158e62ea7b6822ca84fd89de2690c41b2e39
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-11-12 18:31:56 +0600

    Add files via upload

---
+++ CT/CT/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection())
+            {
+                //string isTest = ConfigurationManager.AppSettings["isTest"].ToString();
+                string connectionString = ConfigurationManager.ConnectionStrings["UserConectionString"].ConnectionString;
+                sqlConnection.ConnectionString = connectionString;
+
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+
+                sqlCommand.Connection = sqlConnection;
+
+                sqlCommand.CommandText = "select * from users";
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                
+                while (reader.Read())
+                {
+                    int id = (int)reader["id"];
+                    string login = reader["login"].ToString();
+                    string password = reader["password"].ToString();
+                    //DateTime? updateDate = null;
+                    //if (reader["updateDateTime"] != null)
+                    //{
+                    //    updateDate = DateTime.Parse(reader["updateDateTime"].ToString());
+                    //}
+                }
+
+                reader.Close();
+
+                //sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            }
+            
+        }
+    }
+}
